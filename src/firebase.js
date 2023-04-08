26cc6d0e3b4d7dd7f709913d877a5bd4f55e788e
awesomething <79168736+PairApp@users.noreply.github.com>
2023-04-07 22:17:32 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCnnoDgWvbDT9E0aKOQdUP47U-w2lTZdw",
+  authDomain: "pairapp-67616.firebaseapp.com",
+  projectId: "pairapp-67616",
+  storageBucket: "pairapp-67616.appspot.com",
+  messagingSenderId: "11824657100",
+  appId: "1:11824657100:web:9f9b154e62460d688f0869"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export {initializeApp}
